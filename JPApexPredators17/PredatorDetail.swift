c5313746fad31d639abdc45d42012abaf5fee12a
DiMNDev <Dimndev@gmail.com>
2024-04-25 11:58:57 -0600

    add vstack / alignment / predator name

--- JPApexPredators17/PredatorDetail.swift
+++ JPApexPredators17/PredatorDetail.swift
@@ -11,33 +11,40 @@ struct PredatorDetail: View {
     let predator: ApexPredators
     var body: some View {
         GeometryReader { geo in
-        ScrollView {
-            ZStack(alignment: .bottomTrailing) {
-                // background image
-                Image(predator.type.rawValue)
-                    .resizable()
-                    .scaledToFit()
+            ScrollView {
+                ZStack(alignment: .bottomTrailing) {
+                    // background image
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    // dino image
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/3)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y: 20)
+                }
                 
-                // dino image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: geo.size.width/1.5, height: geo.size.height/3)
-                    .scaleEffect(x: -1)
-                    .shadow(color: .black, radius: 7)
-                    .offset(y: 20)
+                VStack(alignment: .leading) {
+                    
+                    // dino name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    
+                    // current location map
+                    
+                    // appears in list
+                    
+                    // movie moments
+                    
+                    // link to web
+                }
+                .frame(width: geo.size.width, alignment: .leading)
+                .padding()
             }
-            
-            // dino name
-            
-            // current location map
-            
-            // appears in list
-            
-            // movie moments
-            
-            // link to web
-        }
         .ignoresSafeArea()
         }
     }
